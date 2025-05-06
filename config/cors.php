bef8d2643e7cba49e2eece081dd4136bc16ff120
cosmah <cosmahke4@gmail.com>
2025-05-06 19:40:03 +0300

    upadated frontend

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    // 'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [
+        'https://carficautomotiveservice.com',
+        'https://admin.carficautomotiveservice.com',
+    ],
+
 
     'allowed_origins_patterns' => [],
 
