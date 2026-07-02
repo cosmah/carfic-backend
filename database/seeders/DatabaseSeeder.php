c118bca95b11154c50ba38a0f0bde0c04773dde8
cosmah <101720284+cosmah@users.noreply.github.com>
2026-07-02 16:42:34 +0300

    Update DatabaseSeeder to call AdminSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Seed the default admin user
+        $this->call(AdminSeeder::class);
+
+        // Optionally seed a test user
         // User::factory(10)->create();
 
         User::factory()->create([
